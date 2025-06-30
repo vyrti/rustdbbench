5f02b1637c8b07540001eaca55eb8c87a0db7a0d
Aleksei Zaitsev <aleksei.zaitsev@kuehne-nagel.com>
2025-06-30 13:33:13 +0300

    Update

--- kv_benchmark/build.rs
+++ kv_benchmark/build.rs
@@ -1,6 +1,16 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // This tells prost-build to compile the proto file and generate Rust code.
-    // The output will be placed in the `OUT_DIR` directory, which Cargo manages.
     prost_build::compile_protos(&["src/benchmark.proto"], &["src/"])?;
+
+    // This tells flatc to compile the fbs schema and generate Rust code.
+    let fbs_file = "src/benchmark.fbs";
+    println!("cargo:rerun-if-changed={}", fbs_file);
+    flatc_rust::run(flatc_rust::Args {
+        lang: "rust",
+        inputs: &[std::path::Path::new(fbs_file)],
+        out_dir: &std::path::Path::new(&std::env::var("OUT_DIR").unwrap()),
+        ..Default::default()
+    })?;
+
     Ok(())
 }
